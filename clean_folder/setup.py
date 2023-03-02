7b8b9e4205adc52f67de9ac143c6884e7f3173a1
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-03-02 21:45:40 +0200

    hm

---
+++ clean_folder/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    description='Very clean code',
+    author='Illy',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder=clean_folder.clean:clean_folder']}
+)
